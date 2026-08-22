d5277314891077840316789e8eab55b23fd8c6e1
Dulhara Kaushalya <128912391+dulhara79@users.noreply.github.com>
2026-08-22 16:30:07 +0530

    deployment: vendor src/tcwpn/model.py @36d7413, stamp provenance

---
+++ deployment/huggingface/tc_wpn/__init__.py
@@ -0,0 +1,5 @@
+# Vendored from dulhara79/tcwpn_test @ 36d74139069bd3ef99f5289425a63d9d9f3366d3
+# Vendored at 2026-08-22T10:51:49Z by deployment/huggingface/vendor.sh
+# DO NOT EDIT model.py HERE. Edit src/tcwpn/model.py and re-run vendor.sh.
+VENDORED_FROM_COMMIT = "36d74139069bd3ef99f5289425a63d9d9f3366d3"
+VENDORED_AT = "2026-08-22T10:51:49Z"
